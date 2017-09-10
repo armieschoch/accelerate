a3d916f4b1acb0c7a97e9430bed0afa6f342cae3
Armie Schoch <cafeviolette852@gmail.com>
2017-09-09 23:50:24 -0400

    commited changes

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_456');
+define('DB_NAME', ' ');
 
 /** MySQL database username */
-define('DB_USER', 'restWeb_manager');
+define('DB_USER', ' ');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'RestWeb456#@!');
+define('DB_PASSWORD', ' ');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -63,7 +63,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_852';
+$table_prefix  = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
